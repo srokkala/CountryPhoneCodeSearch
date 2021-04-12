45b6b8ba25c79d6bd112d7876b5b84ad0d11ecb3
STEVEN ROKKALA <38511253+srokkala@users.noreply.github.com>
2021-04-12 01:11:37 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import {CountrySearcher} from "countryinfosearch-react-steven";
 
 
 export default function App (){
+    const [number, setNumber] = React.useState();
+    // Testing State Change
+    const getNumber = (data) => {
+        setNumber(data);
+    };
+
     return (
-            <CountrySearcher />
+        <CountrySearcher getPhoneNumber = {getNumber} />
     )
 }
